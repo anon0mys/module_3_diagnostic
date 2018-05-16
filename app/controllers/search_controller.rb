f87f73e6bb285f2ec8bff01dfdad5f3b014943d7
Evan Wheeler <anon0mys87@gmail.com>
2018-05-16 11:15:29 -0600

    Really cool update to service to allow paramsgit add .git add . I know it is late....but it is so cool

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,5 @@
 class SearchController < ApplicationController
   def index
     @presenter = StationsPresenter.new(params[:q])
-    # conn = Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6&fuel_type=ELEC,LPG&limit=10')
-    # response = conn.get do |req|
-    #   req.headers['X-Api-Key'] = ENV['X-API-KEY']
-    # end
-    # query_results = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- app/services/stations_service.rb
+++ app/services/stations_service.rb
@@ -1,19 +1,21 @@
 class StationsService
   def initialize(zip)
     @zip = zip
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json")
   end
 
   def stations_query
-    response = JSON.parse(make_request.body, symbolize_names: true)
+    params = {location: @zip, radius: 6, fuel_type: 'ELEC,LPG', limit: 10}
+    response = JSON.parse(make_request(params).body, symbolize_names: true)
     response[:fuel_stations]
   end
 
   private
 
-  def make_request
-    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&radius=6&fuel_type=ELEC,LPG&limit=10")
-    conn.get do |req|
+  def make_request(params)
+    @conn.get do |req|
       req.headers['X-Api-Key'] = ENV['X-API-KEY']
+      req.params = params
     end
   end
 end
