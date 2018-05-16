a2c088ade20feae2a7009a8a0c65d67406e26c04
Evan Wheeler <anon0mys87@gmail.com>
2018-05-16 10:09:30 -0600

    Updated feature test for station attributes

--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -11,6 +11,18 @@ feature 'User can enter zip into search bar' do
       expect(current_path).to eq(search_path)
 
       expect(page).to have_css('.station-info', count: 10)
+
+      station_info = JSON.parse(File.read('./spec/fixtures/json/single_station.json'), symbolize_names: true)
+
+      station = Station.new(station_info)
+
+      within first('.station-info') do
+        expect(page).to have_content(station.name)
+        expect(page).to have_content(station.full_address)
+        expect(page).to have_content(station.fuel_types)
+        expect(page).to have_content(station.distance)
+        expect(page).to have_content(station.access_times)
+      end
     end
 
     it 'should return a different 10 using a different zip' do
